0cfcb1e1afc7edf4421c070841c3c66dc16d834d
Balram Pandey <balrampandey19@gmail.com>
2017-04-13 16:13:12 +0530

    chrome tab setting

--- app/src/main/java/com/redcarpet/customchrometab/MainActivity.java
+++ app/src/main/java/com/redcarpet/customchrometab/MainActivity.java
@@ -2,12 +2,28 @@ package com.redcarpet.customchrometab;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button=(Button)findViewById(R.id.button);
+
+
+
+
+
     }
+
+
+    private void openChromeTab()
+    {
+
+    }
+
 }
