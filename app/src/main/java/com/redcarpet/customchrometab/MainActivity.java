8caf2980551ba329cdc24745e3a69da15e6a34c0
Balram Pandey <balrampandey19@gmail.com>
2017-04-13 16:16:53 +0530

    add chrome tab open

--- app/src/main/java/com/redcarpet/customchrometab/MainActivity.java
+++ app/src/main/java/com/redcarpet/customchrometab/MainActivity.java
@@ -1,7 +1,11 @@
 package com.redcarpet.customchrometab;
 
+import android.graphics.Color;
+import android.net.Uri;
+import android.support.customtabs.CustomTabsIntent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,17 +17,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button=(Button)findViewById(R.id.button);
-
-
-
-
-
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+openChromeTab();
+            }
+        });
     }
 
 
     private void openChromeTab()
     {
+// Use a CustomTabsIntent.Builder to configure CustomTabsIntent.
+// Once ready, call CustomTabsIntent.Builder.build() to create a CustomTabsIntent
+// and launch the desired Url with CustomTabsIntent.launchUrl()
 
+        String url = "https://paul.kinlan.me/";
+    CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
+        CustomTabsIntent customTabsIntent = builder.build();
+        customTabsIntent.launchUrl(this, Uri.parse(url));
+
+
+        // Changes the background color for the omnibox. colorInt is an int
+// that specifies a Color.
+
+        builder.setToolbarColor(Color.parseColor("#32cd32"));
     }
 
 }
