c82fb4fae543790fcac897b3dc88d697edac9b1e
Balram Pandey <balrampandey19@gmail.com>
2017-04-19 12:21:10 +0530

    add custom close button

--- app/src/main/java/com/redcarpet/customchrometab/MainActivity.java
+++ app/src/main/java/com/redcarpet/customchrometab/MainActivity.java
@@ -1,5 +1,8 @@
 package com.redcarpet.customchrometab;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.net.Uri;
 import android.support.customtabs.CustomTabsIntent;
@@ -11,12 +14,19 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button button;
+    private Bitmap mActionCallIcon, mActionCallLightIcon, mActionBackIcon;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = (Button) findViewById(R.id.button);
+        Resources resources = getResources();
+        mActionCallIcon = BitmapFactory.decodeResource(resources, R.drawable.ic_action_call);
+        mActionCallLightIcon = BitmapFactory.decodeResource(resources, R.drawable.ic_action_call_light);
+        mActionBackIcon = BitmapFactory.decodeResource(resources, R.drawable.ic_action_back);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -29,8 +39,19 @@ public class MainActivity extends AppCompatActivity {
     private void openChromeTab() {
        String url = "https://developer.chrome.com/multidevice/android/customtabs/";
         CustomTabsIntent.Builder builder = new CustomTabsIntent.Builder();
-        builder.setToolbarColor(Color.parseColor("#32cd32"));     CustomTabsIntent customTabsIntent = builder.build();
+        // Set the Toolbar color
+
+        builder.setToolbarColor(Color.parseColor("#32cd32"));
+
+
+
+        // Display custom back button
+
+        builder.setCloseButtonIcon(mActionBackIcon);
+
+        CustomTabsIntent customTabsIntent = builder.build();
         customTabsIntent.launchUrl(this, Uri.parse(url));
+
 
 
     }
